d8c9a0d4d113a08d77da181760b75085a1e4d181
uevgh <uveghobamien@gmail.com>
2025-11-01 18:03:58 +0000

    Add CatalogModule with AddCatalogModule method
    Introduced a new namespace `CatalogModule` containing a static
    class `CatalogModule`. Added a static method `AddCatalogModule`
    to register services using `IServiceCollection`.
    
    Notable issues include:
    - Misspelling of `IServiceCollection` as `IserviceCollection`.
    - Misspelling of `builder.services` as `builder.serviceskm`.
    - Potential infinite recursion due to a recursive call to
      `CatalogModule.AddCatalogModule(builder.services)`.

---
+++ .cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogModule
+{
+    public static class  CatalogModule
+    {
+        public static IserviceCollection AddCatalogModule(  IServiceColleciton services) {
+
+            var builder = WebApplication.CreateBuilder();km
+            //without calling isevice as a native parameter
+            CatalogModule.AddCatalogModule(builder.services)
+            return services
+        }
+        
+    }
+}
