ca8bdaf933f53529c637dcae8f14b193ad6f4d57
uevgh <uveghobamien@gmail.com>
2025-11-03 22:19:46 +0000

    Add EF Core Tools and update metadata
    Added `Microsoft.EntityFrameworkCore.Tools` package to `Shared.csproj` for design-time tasks like migrations. Updated `AssemblyInformationalVersionAttribute` and source link mapping to reflect a new commit hash. Rebuilt binaries (`Shared.dll` and `Shared.pdb`) and updated cache files due to project configuration changes. No functional code changes were made.

--- Shared/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f229ae5da877992127ee60a347ff8d877c7d0390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a76da3eae47e1da27b5a4e131544b4b65571eb08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
